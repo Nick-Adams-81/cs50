500d2938cb975d3168a758309cbca1870b7ab193
Nick Adams <nick.pappas.adams@gmail.com>
2023-10-15 13:34:20 -0700

    user input

--- C/Hello.c
+++ C/Hello.c
@@ -1,7 +1,12 @@
+
 #include <stdio.h>
 
 int main(void)
 {
-    printf("hello world\n");
+    char answer[255];
+    printf("What's your name?\n");
+    fgets(answer, sizeof(answer), stdin);
+    
+    printf("Hello %s", answer);
     return 0;
 }
