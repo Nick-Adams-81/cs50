88b3ee7de7c497eea52b287dd11ed722dfe23fa4
Nick Adams <nick.pappas.adams@gmail.com>
2023-10-16 12:22:59 -0700

    asked first and last name

--- C/Hello.c
+++ C/Hello.c
@@ -3,10 +3,13 @@
 
 int main(void)
 {
-    char answer[255];
-    printf("What's your name?\n");
-    fgets(answer, sizeof(answer), stdin);
-    
-    printf("Hello %s", answer);
+    char first[255];
+    char last[255];
+    printf("What's your first name?\n");
+    fgets(first, sizeof(first), stdin);
+    printf("What is your last name?\n");
+    fgets(last, sizeof(last), stdin);
+
+    printf("Hello, %s%s", first, last);
     return 0;
 }
