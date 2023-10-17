f897fa8886693b52eb0f92239033c6cdc60548e6
Nick Adams <nick.pappas.adams@gmail.com>
2023-10-16 17:49:52 -0700

    for loop

--- C/meow.c
+++ C/meow.c
@@ -2,14 +2,23 @@
 
 int main(void)
 {
-    // printf("meow\n");
-    // printf("meow\n");
-    // printf("meow\n");
     int counter = 5;
     while (counter > 0)
     {
         printf("meow\n");
         counter--;
+    }
+    
+    int i = 0;
+    while (i < 3)
+    {
+        printf("woof\n");
+        i++;
+    }
+
+    for(int i = 0; i < 3; i++)
+    {
+        printf("hello\n");
     }
     return 0;
 }
